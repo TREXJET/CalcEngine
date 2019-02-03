fa4051fe29aade796285c027d6e8fa3476411070
Salik Siddiqui <salik_siddiqui@bose.com>
2019-02-03 17:56:41 -0500

    removing calc cpp and h from the top level...why were they there to begin with

--- Calc.h
+++
@@ -1,9 +0,0 @@
-// Calc.h
-// Author: Salik Siddiqui, July 2018
-
-#include <vector>
-
-void MultiplyVec( const std::vector<long long int> & numList);
-void DivideVec( const std::vector<long long int> & numList );
-
-
